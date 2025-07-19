d5aa9e56bb39e2071c873928f987dde31372145a
gabarrios91 <gabarrios91@gmail.com>
2025-07-19 08:12:48 -0500

    Agregando el codigo para calculadora Basica

---
+++ calculadora.py
@@ -0,0 +1,18 @@
+#funcion calculadora 
+def sumar(a: float, b: float) -> float:
+    #Suma dos números.
+    return a + b
+
+def restar (a: float, b: float) -> float:
+    #Resta el segundo número al primero.
+    return a - b
+
+def multiplicar(a: float, b: float) -> float:
+    #Multiplica dos números.
+    return a * b
+
+def dividir(a: float, b: float) -> float:
+   #Divide el primer número por el segundo.
+    if b == 0:
+        raise ValueError("No se puede dividir por cero.")
+    return a / b
